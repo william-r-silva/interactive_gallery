f37a954d5927937b1b45a713482ad28eb902e3b9
William Rocha Silva <william.rocha.silva@hotmail.com>
2023-05-20 03:15:10 -0300

    Criando servidor

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const multer  = require('multer');
-const upload = multer({ dest: 'images/' });
-
 const app = express();
 
-app.post('/upload', upload.single('photo'), (req, res) => {
-  console.log(req.file);
-  res.send('Arquivo recebido.');
+// Configuração para servir arquivos estáticos
+app.use(express.static('Carroussel'));
+app.use(express.static('Images'));
+app.use(express.static('Assets'));
+
+// Rota para a página inicial
+app.get('/home', (req, res) => {
+  res.sendFile(__dirname +'/Carroussel/home.html');
+  // res.send('Aqui');
 });
 
+// Inicia o servidor
 app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
+  console.log('Servidor está ouvindo na porta 3000...');
 });
