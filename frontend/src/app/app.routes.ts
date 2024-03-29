7205305ced0327dda78db850a88a5b0516e2e1a7
william-r-silva <william.rocha.silva.wrs@gmail.com>
2024-03-29 04:05:02 -0300

    Temp

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-    { path: '/InteractiveGallery', component: AppComponent}
-];
+export const routes: Routes = [];
